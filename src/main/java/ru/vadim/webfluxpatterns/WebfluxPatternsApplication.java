6b87b70a7adab670cde17899633ec391056d2dd6
vadimzakirov <vadimzakiroff@gmail.com>
2026-02-24 23:08:11 +0500

    first commit

---
+++ src/main/java/ru/vadim/webfluxpatterns/WebfluxPatternsApplication.java
@@ -0,0 +1,13 @@
+package ru.vadim.webfluxpatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxPatternsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxPatternsApplication.class, args);
+    }
+
+}
