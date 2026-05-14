2950d1b12994dfc9e896aeab7739cccaac04b690
VadimZakirov <appolon17@gmail.com>
2026-05-14 11:16:12 +0500

    Settings for circuit braker pattern

--- src/main/java/ru/vadim/webfluxpatterns/WebfluxPatternsApplication.java
+++ src/main/java/ru/vadim/webfluxpatterns/WebfluxPatternsApplication.java
@@ -3,7 +3,7 @@ package ru.vadim.webfluxpatterns;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "ru.vadim.webfluxpatterns.sec07")
+@SpringBootApplication(scanBasePackages = "ru.vadim.webfluxpatterns.sec08")
 public class WebfluxPatternsApplication {
 
     public static void main(String[] args) {
